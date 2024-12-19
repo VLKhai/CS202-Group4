a94ee8fb82f78f1d85ca21a860f2ee3bb2d5fde4
Kai <146848332+VLKhai@users.noreply.github.com>
2024-12-19 23:38:28 +0700

    add Hammer.h and Hammer.cpp

---
+++ groupProject/Hammer.cpp
@@ -0,0 +1,82 @@
+#include "Hammer.h"
+#include "Core.h"
+
+/* ******************************************** */
+
+Hammer::Hammer(int iXPos, int iYPos, bool moveDirection) {
+	this->fXPos = (float)iXPos + 4;
+	this->fYPos = (float)iYPos + 4;
+
+	this->iBlockID = Core::getMap()->getLevelType() == 0 || Core::getMap()->getLevelType() == 4 ? 47 : 48;
+
+	this->iHitBoxX = 24;
+	this->iHitBoxY = 24;
+
+	this->moveDirection = moveDirection;
+	this->jumpState = 1;
+	this->jumpDistance = 48;
+	this->currentJumpDistance = 0;
+	this->moveSpeed = 0;
+	this->minionState = 0;
+
+	this->collisionOnlyWithPlayer = true;
+
+	this->minionSpawned = true;
+}
+
+Hammer::~Hammer(void) {
+
+}
+
+/* ******************************************** */
+
+void Hammer::Update() {
+	if(minionState != -2) {
+		++moveSpeed;
+		if(moveSpeed > 35) {
+			fXPos += moveDirection ? 3 : -3;
+		} else if(moveSpeed > 15) {
+			fXPos += moveDirection ? 2.5f : -2.5f;
+		} else {
+			fXPos += moveDirection ? 2 : -2;
+		}
+	} else {
+		Minion::minionDeathAnimation();
+	}
+}
+
+void Hammer::Draw(sf::RenderWindow& window, IMG* iIMG) {
+	if(minionState != -2) {
+		iIMG->draw(window, (int)fXPos + (int)Core::getMap()->getXPos() - 4, (int)fYPos - 4, false);
+	} else {
+		iIMG->drawVert(window, (int)fXPos + (int)Core::getMap()->getXPos() - 4, (int)fYPos - 4);
+	}
+}
+
+void Hammer::minionPhysics() {
+	if (jumpState == 1) {
+		Minion::physicsState1();
+	} else {
+		currentFallingSpeed *= 1.06f;
+
+		if (currentFallingSpeed > startJumpSpeed) {
+			currentFallingSpeed = startJumpSpeed;
+		}
+
+		fYPos += currentFallingSpeed;
+
+		if (fYPos >= CFG::GameHeight) {
+			minionState = -1;
+		}
+	}
+}
+
+/* ******************************************** */
+
+void Hammer::collisionWithPlayer(bool TOP) {
+	if(Core::getMap()->getPlayer()->getStarEffect()) {
+		setMinionState(-2);
+	} else {
+		Core::getMap()->playerDeath(true, false);
+	}
+}
---
+++ groupProject/Hammer.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#ifndef HAMMER_H
+#define HAMMER_H
+
+#include "Minion.h"
+
+class Hammer : public Minion
+{
+private:
+
+public:
+	Hammer(int iXPos, int iYPos, bool moveDirection);
+	~Hammer(void);
+
+	void Update();
+	void Draw(sf::RenderWindow& window, IMG* iIMG);
+
+	void minionPhysics();
+
+	void collisionWithPlayer(bool TOP);
+};
+
+#endif
