e5790208fc8bc611da6a5193903b696ed1ed9adc
Kai <146848332+VLKhai@users.noreply.github.com>
2024-12-20 00:07:31 +0700

    add Mushroom.h and Mushroom.cpp

---
+++ groupProject/Mushroom.cpp
@@ -0,0 +1,83 @@
+#include "Mushroom.h"
+#include "Core.h"
+
+/* ******************************************** */
+
+Mushroom::Mushroom(int iXPos, int fYPos, bool powerUP, int iX, int iY) {
+	this->fXPos = (float)iXPos;
+	this->fYPos = (float)fYPos - 2;
+
+	this->iBlockID = powerUP ? 2 : Core::getMap()->getLevelType() == 1 ? 25 : 3;
+	this->moveSpeed = 2;
+	this->inSpawnState = true;
+	this->minionSpawned = true;
+	this->inSpawnY = 30;
+	this->moveDirection = false;
+	this->powerUP = powerUP;
+	this->collisionOnlyWithPlayer = true;
+
+	this->minionState = 0;
+
+	this->iX = iX;
+	this->iY = iY;
+}
+
+Mushroom::~Mushroom(void) {
+
+}
+
+/* ******************************************** */
+
+void Mushroom::Update() {
+	if (inSpawnState) {
+		if (inSpawnY <= 0) {
+			inSpawnState = false;
+		} else {
+			if (fYPos > -5) {
+				inSpawnY -= 2;
+				fYPos -= 2;
+			} else {
+				inSpawnY -= 1;
+				fYPos -= 1;
+			}
+		}
+	} else {
+		updateXPos();
+	}
+}
+
+bool Mushroom::updateMinion() {
+	if (!inSpawnState) {
+		minionPhysics();
+	}
+
+	return minionSpawned;
+}
+
+void Mushroom::Draw(sf::RenderWindow& window, IMG* iIMG) {
+	if(minionState >= 0) {
+		iIMG->draw(window, (int)fXPos + (int)Core::getMap()->getXPos(), (int)fYPos + 2, false);
+		if (inSpawnState) {
+			Core::getMap()->getBlock(Core::getMap()->getLevelType() == 0 || Core::getMap()->getLevelType() == 4 ? 9 : 56)->getSprite()->getTexture()->draw(window, (int)fXPos + (int)Core::getMap()->getXPos(), (int)fYPos + (32 - inSpawnY) - Core::getMap()->getMapBlock(iX, iY)->getYPos() + 2, false);
+		}
+	}
+}
+
+/* ******************************************** */
+
+void Mushroom::collisionWithPlayer(bool TOP) {
+	if(!inSpawnState && minionState == 0) {
+		if(powerUP) {
+			Core::getMap()->getPlayer()->setPowerLVL(Core::getMap()->getPlayer()->getPowerLVL() + 1);
+		} else {
+			Core::getMap()->getPlayer()->setNumOfLives(Core::getMap()->getPlayer()->getNumOfLives() + 1);
+			Core::getMap()->addPoints((int)fXPos, (int)fYPos, "1UP", 10, 14);
+			CFG::getMusic()->PlayChunk(CFG::getMusic()->cONEUP);
+		}
+		minionState = -1;
+	}
+}
+
+void Mushroom::setMinionState(int minionState) {
+	
+}
