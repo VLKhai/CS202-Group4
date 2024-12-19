c36f669b2ea0133778df09197b79de30b3183f64
Kai <146848332+VLKhai@users.noreply.github.com>
2024-12-20 00:13:33 +0700

    add Spikey.h and Spikey.cpp

---
+++ groupProject/Spikey.cpp
@@ -0,0 +1,69 @@
+#include "Spikey.h"
+#include "Core.h"
+
+/* ******************************************** */
+
+Spikey::Spikey(int iXPos, int iYPos) {
+	this->fXPos = (float)iXPos;
+	this->fYPos = (float)iYPos + 6;
+
+	this->iHitBoxX = 32;
+	this->iHitBoxY = 26;
+
+	this->iBlockID = 52;
+
+	this->startJump(1);
+
+	this->moveSpeed = 0;
+}
+
+Spikey::~Spikey(void) {
+
+}
+
+/* ******************************************** */
+
+void Spikey::Update() {
+	if (minionState == 0) {
+		updateXPos();
+	} else if(minionState == -2) {
+		Minion::minionDeathAnimation();
+	}
+}
+
+void Spikey::Draw(sf::RenderWindow& window, IMG* iIMG) {
+	if(minionState != -2) {
+		iIMG->draw(window, (int)fXPos + (int)Core::getMap()->getXPos(), (int)fYPos - 4, !moveDirection);
+	} else {
+		iIMG->drawVert(window, (int)fXPos + (int)Core::getMap()->getXPos(), (int)fYPos + 2);
+	}
+}
+
+void Spikey::minionPhysics() {
+	if (jumpState == 1) {
+		physicsState1();
+	} else {
+		if (!Core::getMap()->checkCollisionLB((int)fXPos + 2, (int)fYPos + 2, iHitBoxY, true) && !Core::getMap()->checkCollisionRB((int)fXPos - 2, (int)fYPos + 2, iHitBoxX, iHitBoxY, true) && !onAnotherMinion) {
+			physicsState2();
+		} else {
+			jumpState = 0;
+			onAnotherMinion = false;
+
+			if(iBlockID == 52) {
+				iBlockID = 51;
+				moveDirection = fXPos + iHitBoxX/2 > Core::getMap()->getPlayer()->getXPos() - Core::getMap()->getXPos() + Core::getMap()->getPlayer()->getHitBoxX()/2;
+				moveSpeed = 1;
+			}
+		}
+	}
+}
+
+/* ******************************************** */
+
+void Spikey::collisionWithPlayer(bool TOP) {
+	if(Core::getMap()->getPlayer()->getStarEffect()) {
+		setMinionState(-2);
+	} else {
+		Core::getMap()->playerDeath(true, false);
+	}
+}
---
+++ groupProject/Spikey.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#ifndef SPIKEY_H
+#define SPIKEY_H
+
+#include "Minion.h"
+
+class Spikey : public Minion
+{
+private:
+
+public:
+	Spikey(int iXPos, int iYPos);
+	~Spikey(void);
+
+	void Update();
+	void Draw(sf::RenderWindow& window, IMG* iIMG);
+
+	void minionPhysics();
+
+	void collisionWithPlayer(bool TOP);
+};
+
+#endif
