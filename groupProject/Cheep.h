c7ec695d8bf7eebf698a44b9604a9926716eafc1
Kai <146848332+VLKhai@users.noreply.github.com>
2024-12-19 23:55:34 +0700

    add Cheep.h and Cheep.cpp

---
+++ groupProject/Cheep.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#ifndef CHEEP_H
+#define CHEEP_H
+
+#include "Minion.h"
+
+class Cheep : public Minion
+{
+private:
+	bool moveY, moveYDIR;
+public:
+	Cheep(int iXPos, int iYPos, int minionType, int moveSpeed, bool moveDiretion = false);
+	~Cheep(void);
+
+	void Update();
+	void Draw(sf::RenderWindow& window, IMG* iIMG);
+	void minionPhysics();
+	void collisionWithPlayer(bool TOP);
+};
+
+#endif
