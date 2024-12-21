d1357077ca1a9e7be63de4748b8e10dacc4de72c
VtHien <23125056@student.hcmus.edu.vn>
2024-12-21 09:05:53 +0700

    FireBall Implementation

---
+++ groupProject/FireBall.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#ifndef FIREBALL_H
+#define FIREBALL_H
+
+#include "Minion.h"
+
+class FireBall : public Minion
+{
+private:
+	int radius; // -- r
+	double angle;
+
+	int iCenterX, iCenterY;
+
+	double slice;
+
+	int sliceID;
+
+public:
+	FireBall(int iXPos, int iYPos, int radius, int nSliceID, bool moveDirection);
+	~FireBall(void);
+
+	void Draw(sf::RenderWindow& window, IMG* iIMG);
+	void Update();
+
+	void updateXPos();
+
+	void collisionWithPlayer(bool TOP);
+
+	void minionPhysics();
+};
+
+#endif
