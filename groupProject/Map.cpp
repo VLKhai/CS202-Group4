b0cc6cdc8a163b4b2e8813061224c0a3f51dbbe2
izahai <155749684+izahai@users.noreply.github.com>
2024-12-27 23:14:30 +0700

    2 player fixx

--- groupProject/Map.cpp
+++ groupProject/Map.cpp
@@ -55,24 +55,22 @@ void Map::notify(Minion* pmSender, std::string sEvent)
 		vCurPlayer[1]->startJump(1);
 		return;
 	}
-	if (sEvent == "P0_star" || sEvent == "P1_star") {
+	if (sEvent == "P0_star") {
 		vCurPlayer[0]->setStarEffect(true);
+		return;
+	}
+	if (sEvent == "P1_star") {
 		vCurPlayer[1]->setStarEffect(true);
 		return;
 	}
-	//if (sEvent == "P1_star") {
-	//	vCurPlayer[1]->setStarEffect(true);
-	//	return;
-	//}
-	if (sEvent == "P0_LVUP" || sEvent == "P1_LVUP") {
+	if (sEvent == "P0_LVUP") {
 		vCurPlayer[0]->setPowerLVL(vCurPlayer[0]->getPowerLVL() + 1);
+		return;
+	}
+	if (sEvent == "P1_LVUP") {
 		vCurPlayer[1]->setPowerLVL(vCurPlayer[1]->getPowerLVL() + 1);
 		return;
 	}
-	//if (sEvent == "P1_LVUP") {
-	//	vCurPlayer[1]->setPowerLVL(vCurPlayer[1]->getPowerLVL() + 1);
-	//	return;
-	//}
 	if (sEvent == "LIVEUP") {
 		for (Player* tmp : vCurPlayer) {
 			tmp->setNumOfLives(tmp->getNumOfLives() + 1);
